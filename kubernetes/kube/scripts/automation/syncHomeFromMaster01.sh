735e4345a8d5479522f7d25c29b42cf63cc83383
Ryan <mccullough.ryan123@gmail.com>
2023-10-08 08:09:40 +0000

    Apparently had the services thing wrong, updated to reflect what I needed to add

---
+++ kubernetes/kube/scripts/automation/syncHomeFromMaster01.sh
@@ -0,0 +1,3 @@
+sudo rsync -azvP --exclude=".[a-z]*" /home/rmccu/ rmccu@k8s-master-02:/home/rmccu
+sudo rsync -azvP --exclude=".[a-z]*" /home/rmccu/ rmccu@k8s-master-03:/home/rmccu
+
---
+++ kubernetes/kube/scripts/automation/syncHomeFromMaster03.sh
@@ -0,0 +1,2 @@
+rsync -azvP --exclude=".[a-z]*" /home/rmccu/ rmccu@k8s-master-01:/home/rmccu
+rsync -azvP --exclude=".[a-z]*" /home/rmccu/ rmccu@k8s-master-02:/home/rmccu
